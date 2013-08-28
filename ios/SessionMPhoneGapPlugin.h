26ef01f092a77e392b6e604ba4b262dc15f115fc
Noah Durell <ndurell@sessionm.com>
2013-08-28 13:43:28 -0400

    ios files.

---
+++ ios/SessionMPhoneGapPlugin.h
@@ -0,0 +1,14 @@
+//
+//  SessionMPhoneGapPlugin.h
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDV.h>
+#import "SessionM.h"
+
+@interface SessionMPhoneGapPlugin : CDVPlugin<SessionMDelegate> {
+    
+}
+
+@end
